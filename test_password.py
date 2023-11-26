c12651f4f46c98b81045498c73643b389ac4de85
ricardotakahashi145 <70351071+ricardotakahashi145@users.noreply.github.com>
2023-11-26 22:37:55 +0000

    atualização de test_password

--- test_password.py
+++ test_password.py
@@ -7,12 +7,18 @@ def test_password(test, correct):
 
 with open ('correct.txt','r') as password_f:
     test = password_f.read()
-    test = hashlib.sha256(test.encode('utf-8')).hexdigest()
+    test_hash = hashlib.sha256(test.encode('utf-8')).hexdigest()
 
 with open ('hash.txt', 'r') as hash_f:
     hash = hash_f.read()
 
-if (test_password(test, hash)):
+with open('correct.txt', 'r') as correct_f:
+    correct = correct_f.read()
+
+print('A senha lida é:', test)
+print('A senha correta é:', correct)
+
+if (test_password(test_hash, hash)):
     print('A senha está correta!')
 else:
     print('A senha está incorreta!')
